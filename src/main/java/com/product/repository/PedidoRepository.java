284bbb3a36841f8afb5b935d73510b7da33c9ef1
leilanyaragao <leilanyaragao26@gmail.com>
2025-05-28 21:19:17 -0300

    add repository

---
+++ src/main/java/com/product/repository/PedidoRepository.java
@@ -0,0 +1,10 @@
+package com.product.repository;
+
+
+import com.product.model.Pedido;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoRepository extends MongoRepository<Pedido, Integer> {
+}
