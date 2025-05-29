dc40d8b6d84f38736930b04eb4e12045796e5f83
leilanyaragao <leilanyaragao26@gmail.com>
2025-05-28 21:58:22 -0300

    fix

--- src/main/java/com/product/model/CaixaUsada.java
+++ src/main/java/com/product/model/CaixaUsada.java
@@ -1,10 +1,14 @@
 package com.product.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CaixaUsada {
     private String caixa_id;
     private List<String> produtos;
--- src/main/java/com/product/model/Produto.java
+++ src/main/java/com/product/model/Produto.java
@@ -1,8 +1,12 @@
 package com.product.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Produto {
     private String produto_id;
     private Dimensoes dimensoes;
